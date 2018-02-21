72282a70bcd07b67e6f87f7ed4ff9fae662d48a2
lilian <lilian@edadeal.ru>
2018-02-21 17:03:28 +0300

    wip: implement calculator

--- index.js
+++ index.js
@@ -8,67 +8,97 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-  res.json({ version: packageInfo.version });
+    res.json({
+        version: packageInfo.version
+    });
 });
 
 var server = app.listen(process.env.PORT, "0.0.0.0", () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log('Web server started at http://%s:%s', host, port);
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log('Web server started at http://%s:%s', host, port);
 });
 
-function key(n) {
-    return {"text": n, "callback_data": n}
+function key(n, cb) {
+    // if (cb == '') {cb == n}
+    return {
+        "text": n,
+        "callback_data": cb + n
+    }
 }
 
-function make_keys() {
+function make_keys(cb) {
     var res = []
     var keys_list = [
         ['AC', '+', '-'],
-        ['7', '8', '9'], 
-        ['4', '5', '6'], 
-        ['1', '2', '3'], 
-        [' ', '0' ,' '],
+        ['7', '8', '9'],
+        ['4', '5', '6'],
+        ['1', '2', '3'],
+        [' ', '0', ' '],
     ]
-    keys_list.forEach(function(value_list){
-        var current_json = []
-        value_list.forEach(function(value){
-          current_json.push(key(value));
-        })
+    keys_list.forEach(function (value_list) {
+        var current_json = [];
+        value_list.forEach(value => current_json.push(key(value, cb)));
         res.push(current_json);
     });
     return res
 }
 
-function handle_message(req) {
-  res.send({
-      'method':'sendMessage',
-      'chat_id':req.body.callback_query.message.chat.id,
-      'text':'hello',
-      'reply_markup':{
-          'inline_keyboard': make_keys()
-      }
-  });
+function handle_message(req, res) {
+    var cb = ''
+    res.send({
+        'method': 'sendMessage',
+        'text': '0',
+        'chat_id': req.body.message.chat.id,
+        'reply_markup': {
+            'inline_keyboard': make_keys(cb)
+        }
+    });
 
 }
 
-function handle_callback(req) {
-  res.send({
-      'method':'update',
-      'chat_id':req.body.callback_query.message.chat.id,
-      'text':req.body.callback_query.message.chat.text,
-      'reply_markup':{
-          'inline_keyboard': make_keys()
-      }
-  });
+function handle_callback(req, res) {
+    var mt
+    var cb
 
+    if (req.body.callback_query.data == '+' || req.body.callback_query.data == '-'){
+        mt = req.body.callback_query.message.text; 
+        cb = req.body.callback_query.data;
+    }
+    else if (req.body.callback_query.data == "++" || req.body.callback_query.data == "--" ||req.body.callback_query.data == "-+" ||req.body.callback_query.data == "+-"){
+        mt = req.body.callback_query.message.text; 
+        cb = req.body.callback_query.data.slice(-1);
+    }
+    else if (req.body.callback_query.message.text.match(/\d+[\+\-]\d+[\+\-]/)) {
+        mt = eval(req.body.callback_query.message.text.slice(0,-2));
+        cb = req.body.callback_query.message.text.slice(-1);
+    }
+    else if (!isNaN(req.body.callback_query.data)){
+        if (req.body.callback_query.message.text != '0'){
+        mt = req.body.callback_query.message.text + req.body.callback_query.data;
+        } 
+        else {mt = req.body.callback_query.data;}
+        cb = ''
+    }
+    else {mt = 0; cb = ''}
+
+    res.send({
+        'method': 'editMessageText',
+        'chat_id': req.body.callback_query.message.chat.id,
+        'message_id': req.body.callback_query.message.message_id,
+        'text': mt,
+        'reply_markup': {
+            'inline_keyboard': make_keys(cb)
+        }
+    });
 }
 
 app.post('/' + process.env['WEBHOOK_URL'], (req, res) => {
-  console.log(req.body);
-  if calback_query in req.body:
-    handle_callback(req)
-    #  if message in req.body:
-    #  handle_message(req)
+    console.log(req.body);
+    if (req.body.callback_query != undefined) {
+        handle_callback(req, res)
+    }
+    if (req.body.message != undefined) {
+        handle_message(req, res)
+    }
 });
-
